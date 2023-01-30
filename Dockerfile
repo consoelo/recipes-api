95fdf339875b8020f1dde7bee5cd5cca17c6091e
“mew” <johnnysales_23@hotmail.com>
2023-01-30 20:42:55 -0300

    Implements wait_for_db command.

--- Dockerfile
+++ Dockerfile
@@ -22,7 +22,15 @@ RUN python -m venv /py && \
     fi && \
     rm -rf /tmp && \
     apk del .tmp-build-deps && \
-    adduser --disabled-password --no-create-home django-user
+    adduser \
+        --disabled-password \
+        --no-create-home \
+        django-user && \
+    mkdir -p /vol/web/media && \
+    mkdir -p /vol/web/static && \
+    chown -R django-user:django-user /vol && \
+    chmod -R 755 /vol && \
+    chmod -R +x /scripts
 
 ENV PATH="/scripts:/py/bin:$PATH"
 
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -21,7 +21,7 @@ class CommandTests(SimpleTestCase):
 
         call_command('wait_for_db')
 
-        patched_check.assert_called_once_with(database=['default'])
+        patched_check.assert_called_once_with(databases=['default'])
 
 
     @patch('time.sleep')
@@ -32,5 +32,5 @@ class CommandTests(SimpleTestCase):
 
         call_command('wait_for_db')
 
-        self.assertEqual(patched_check.call_couut, 6)
-        patched_check.assert_called_with(database=['default'])
+        self.assertEqual(patched_check.call_count, 6)
+        patched_check.assert_called_with(databases=['default'])
